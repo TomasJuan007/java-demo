c4588f640ccf81c0e252a3a0443c5e944d4e713c
Tomas <tomas.huang@ericsson.com>
2017-12-29 19:08:00 +0800

    Refactor project

---
+++ project/src/main/java/com/example/demo/service/impl/RedisServiceImpl.java
@@ -0,0 +1,40 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.RedisService;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by etomhua on 10/30/2017.
+ */
+@Service
+public class RedisServiceImpl implements RedisService {
+    private StringRedisTemplate template;
+
+    public RedisServiceImpl(StringRedisTemplate template) {
+        this.template = template;
+    }
+
+    @Override
+    public Map<String, String> sayHello() {
+        Map<String, String> keyPairs = new HashMap<String, String>();
+        Set<String> keys = new HashSet<>();/*template.keys("*")*/;
+        keys.add("rpk_ep00251");
+        keys.add("rpk_ep00tom");
+        for(String key : keys) {
+            ValueOperations<String, String> operation = template.opsForValue();
+            String value = operation.get(key);
+            keyPairs.put(key, value);
+        }
+        return keyPairs;
+    }
+}
--- src/test/resources/create.sql
+++
@@ -1,9 +0,0 @@
-create table test (
-    id varchar(255) not null,
-    header varchar(255),
-    type varchar(255),
-    playload varchar(255),
-    primary key (id)
-);
-
-INSERT INTO test (id, header, type, playload) VALUES ('1', 'tenant:mobile', 'string', 'testing');
