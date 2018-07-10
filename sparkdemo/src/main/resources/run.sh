2053efb160688b2680632febc5c5e7044b0174ca
Tomas <tomas.huang@ericsson.com>
2018-07-10 16:04:37 +0800

    Fix run shell script

--- sparkdemo/src/main/resources/run.sh
+++ sparkdemo/src/main/resources/run.sh
@@ -4,10 +4,10 @@
 _bindir=`dirname $0`
 _basedir=`cd ${_bindir}/.. && pwd`
 
-LIB="${_basedir}/lib/*"
+LIB="${_basedir}/dependency/*"
 
 # java options
 JAVA_OPTS="-server -Xms128m -Xmx256m"
-JAVA_ARGS="sparkdemo/src/main/resources/run.sh sparkdemo/src/main/resources/dump"
+JAVA_ARGS="${_basedir}/classes/run.sh" "${_basedir}/dump"
 
-java ${JAVA_OPTS} -cp "${LIB}" -Dspark.master=local com.example.demo.WordCount JAVA_ARGS "$@"
+java ${JAVA_OPTS} -cp "${LIB}" -Dspark.master=local com.example.demo.WordCount ${JAVA_ARGS}
