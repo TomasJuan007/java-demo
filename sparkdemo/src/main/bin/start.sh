029d48e911e9c13daa76ad8e56b3d6099ef0f365
Tomas <tomas.huang@ericsson.com>
2018-08-27 19:39:40 +0800

    Fix start.sh for spark demo

--- sparkdemo/src/main/bin/start.sh
+++ sparkdemo/src/main/bin/start.sh
@@ -4,10 +4,11 @@
 _bindir=`dirname $0`
 _basedir=`cd ${_bindir}/.. && pwd`
 
-LIB="${_basedir}/dependency/*"
+LIB="${_basedir}/lib/*"
 
 # java options
-JAVA_OPTS="-server -Xms128m -Xmx256m"
-JAVA_ARGS="${_basedir}/classes/run.sh" "${_basedir}/dump"
+JAVA_OPTS="-server -Xms128m -Xmx512m"
+SOURCE="${_basedir}/config/application-test.properties"
+RESULT_DIR="${_basedir}/dump"
 
-java ${JAVA_OPTS} -cp "${LIB}" -Dspark.master=local com.example.demo.WordCount ${JAVA_ARGS}
+java ${JAVA_OPTS} -cp "${LIB}" -Dspark.master=local com.example.demo.WordCount "$SOURCE" "$RESULT_DIR"
