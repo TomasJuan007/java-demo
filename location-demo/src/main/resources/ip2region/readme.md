972e5c8965b551c71aedb3bead69a786616cfb04
Tomas <18819451598@163.com>
2023-07-01 00:55:23 +0800

    location demo

--- location-demo/src/main/resources/ip2region/readme.md
+++ location-demo/src/main/resources/ip2region/readme.md
@@ -8,18 +8,25 @@
 数据来源自apnic，使用Apnic2StdConverter处理为ip.merge.txt格式，再转换为xdb文件
 
 ## xdb文件生成方式
-> 1. 获取最新数据
+> 1.获取最新数据
 > 
 > curl http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest -o ip.apnic-source.txt
 
-> 2. 生成ip2region可以处理的格式
+> 2.生成ip2region可以处理的格式
 >
 > Apnic2StdConverter.main()生成ip.merge_apnic.txt
+>
+> or
+>
+> cat ip.apnic-source.txt | awk -F '|' '/CN/&&/ipv4/ {print $4 "|" f($4, $5) "|" $2}' | cat > ipv4.txt
  
-> 3. 生成xdb文件
+> 3.生成xdb文件
 >
 > java -jar ip2region-maker-1.0.0.jar --src=${src} --dst=${dst}
 >
 > 示例：
 >
 > java -jar ip2region-maker-1.0.0.jar --src=./apnic/ip.merge_apnic.txt  --dst=./apnic/ip2region_apnic.xdb
+
+# 参考资料
+[一文搞懂全网上线的 IP 属地功能](https://zhuanlan.zhihu.com/p/515338201)
--- location-demo/src/test/java/com/location/demo/utils/IpUtilsTest.java
+++ location-demo/src/test/java/com/location/demo/utils/IpUtilsTest.java
@@ -9,19 +9,11 @@ public class IpUtilsTest {
     @Test
     public void testGetIpAddressInfo() {
         List<String> ipList = new ArrayList<>();
-        ipList.add("1.119.142.106");
-        ipList.add("1.119.145.146");
-        ipList.add("1.119.157.42");
-        ipList.add("1.119.166.58");
-        ipList.add("1.119.197.50");
-        ipList.add("1.145.239.91");
-        ipList.add("1.147.78.105");
-        ipList.add("1.152.21.163");
-        ipList.add("1.152.31.82");
-        ipList.add("1.160.149.73");
-        ipList.add("1.162.132.183");
-        ipList.add("1.162.164.124");
         ipList.add("1.163.214.65"); //TW
+        ipList.add("8.134.42.147");
+        ipList.add("8.134.42.149");
+        ipList.add("103.170.73.12");
+        ipList.add("139.198.120.23");
         for (String ip : ipList) {
             String i = IpUtils.getIpAddressInfo(ip);
             System.out.println(i);
